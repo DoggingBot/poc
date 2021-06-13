0267ba5a9b8a432ce36d18ecc7394498345046f9
Donald Carnegie <donald.carnegie@external.total.com>
2021-06-13 08:42:38 +0100

    twizzle

--- src/commands/sipCommand.js
+++ src/commands/sipCommand.js
@@ -20,6 +20,10 @@ async function handle(message) {
         msgString += ". Nice.";
     }
 
+    if (userObj.count == 100) {
+        msg = "100 sips is a proud moment for any pisshead. Sir pisshead of sip, I hereby grant you one sipcoin and all the rights and lands associated with the title. Twizzle may not be the first, but he Flushed all the Jacksons in his way, and is thus the first owner of a sipcoin."
+    }
+
     message.channel.send(msgString);
 }
 
